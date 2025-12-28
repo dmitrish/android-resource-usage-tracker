cfed8ecd1de58afd71ebc5fc55eaf346b9592dc2
GitHub Action <action@github.com>
2025-12-28 19:23:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/dmitrish/androidresourceusagetracker/MyBundle.kt
+++ src/main/kotlin/com/github/dmitrish/androidresourceusagetracker/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dmitrish.androidresourceusagetracker
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dmitrish/androidresourceusagetracker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dmitrish/androidresourceusagetracker/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.dmitrish.androidresourceusagetracker.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dmitrish.androidresourceusagetracker.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
